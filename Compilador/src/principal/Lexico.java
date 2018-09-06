acdd9533adedbf2bc4f19de3256448bfc577ecfe
Pablo <Pablo@Dell-Notebook>
2018-09-06 18:23:46 -0300

    c2

---
+++ Compilador/src/principal/Lexico.java
@@ -0,0 +1,22 @@
+package principal;
+
+import java.io.BufferedReader;
+import java.util.Hashtable;
+
+public class Lexico {
+	/*int matrizEstados[][] = {...};
+	AccionSemantica matrizAS[][] = {...};
+	Hashtable<String,Simbolo> tablaSimbolos = new Hashtable...;
+	BufferedReader codigoFuente;
+	...
+	public int getToken() {
+		estadoActual = 0;
+		while (estadoActual != -1){
+			caracterActual = codigoFuente.read();
+			as = martizAS[estadoActual][caracterActual];
+			tokenId = as.ejecutar(...);
+			estadoActual = matrizEstados[estadoActual][caracterActual];
+		}
+	return tokenId;
+	}*/
+}
---
+++ Compilador/src/principal/Parser.java
@@ -0,0 +1,17 @@
+package principal;
+
+public class Parser {
+	
+/*	private Lexico analizadorLexico;
+	
+	public Parser(analizadorLexico) {
+		this.analizadorLexico = analizadorLexico;
+		token = this.analizadorLexico.getToken();
+		while (token != -1){
+			token = this.analizadorLexico.getToken();
+			System.out.println(token);
+		}
+	this.analizadorLexico.mostrarTablaSimbolos();
+	}
+*/
+}
