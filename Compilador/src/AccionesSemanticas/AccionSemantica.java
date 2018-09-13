824c31d85af265a459c44046e6a51335d7bbf29e
SebasLanz <sebastian_lanz@hotmail.com>
2018-09-13 19:05:49 -0300

    das

--- Compilador/src/AccionesSemanticas/AccionSemantica.java
+++ Compilador/src/AccionesSemanticas/AccionSemantica.java
@@ -1,5 +1,5 @@
 package AccionesSemanticas;
-//es solo de definición casa AS sobreescribe el metodo
+//es solo de definiciï¿½n casa AS sobreescribe el metodo
 public abstract class AccionSemantica {
 	
 	public String ejecutar() {
