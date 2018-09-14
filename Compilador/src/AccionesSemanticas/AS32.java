ae77e2f305c39e387b70306078a75ebf4721c8fd
herna <herna@MSI>
2018-09-14 01:17:29 -0300

    AS32

---
+++ Compilador/src/AccionesSemanticas/AS32.java
@@ -0,0 +1,10 @@
+package AccionesSemanticas;
+
+public class AS32 extends AccionSemantica {
+	
+	public void ejecutar(Character carActual, String tokenString, int cantLin ){
+		this.tokenString = tokenString + Character.toString(carActual);
+		devuelveChar = false;
+	}
+
+}
